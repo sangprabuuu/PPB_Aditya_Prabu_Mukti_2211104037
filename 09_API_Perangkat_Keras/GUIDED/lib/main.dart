efea0ff7de9919e52c9ece7810d1d5db45f61fba
sangprabuuu <aditmuhti@gmail.com>
2024-11-14 00:01:09 +0700

    09_API_Perangkat_Keras

---
+++ 09_API_Perangkat_Keras/GUIDED/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// import 'package:pertemuan9_2/camera_screen.dart';
+import 'package:pertemuan9_2/image_picker_screen.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ImagePickerScreen(type: ImageSourceType.gallery),
+    );
+  }
+}
